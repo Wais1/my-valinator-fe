f572cbc62671c86fdc2f11100d721eaa1d8a37fb
Wais1 <waisibrahim77@gmail.com>
2022-05-13 11:31:55 +0700

    completely working FE implementation with no rooms

--- public/content.js
+++ public/content.js
@@ -5,16 +5,20 @@
 // Animixplay play button, that hardly works
 // var playbtn = document.getElementsByClassName("plyr__controls__item plyr__control")[1]
 
+// Should probably be here
+//  const playbtn = document.getElementsByClassName("ytp-play-button ytp-button")[0]
+    // const playbtn2 = document.getElementsByClassName("ytp-play-button ytp-button")[1]
 
 // Define pause button
 const pause = () =>{
   // Youtube play button WORKS with youtube
-  // MAYBE ITS BEING CALLED BEFORE ITS FULLY LOADED AND THEREFORE RANDOMLY WORKS OR DOESNT
   const playbtn = document.getElementsByClassName("ytp-play-button ytp-button")[0]
   
   // Experimental (press both buttons, not just one. diff on diff machines)
   const playbtn2 = document.getElementsByClassName("ytp-play-button ytp-button")[1]
+  // MAYBE ITS BEING CALLED BEFORE ITS FULLY LOADED AND THEREFORE RANDOMLY WORKS OR DOESNT
 
+  // check if playbtn returns something (clg). check if undefined. if undefined, return (while undefined)
   playbtn.click()
   // Experimental: pressing both buttons.
   playbtn2.click()
